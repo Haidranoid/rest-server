edd3b1077144965026af9ace175dbf9537dddf5c
Jose Eduardo Martinez Lara <martinezlara_joseeduardo@hotmail.com>
2020-09-16 22:38:20 -0500

    uploads to server advance 1

--- server/routes/api-uploads.js
+++ server/routes/api-uploads.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const uniqid = require('uniqid');
 const verifyFile = require('../middlewares/verifyFile');
 const app = express();
 
@@ -7,25 +8,43 @@ app.put('/api/uploads/:folder/:id', verifyFile, (req, res) => {
 
     switch (folder) {
         case 'users':
+            const {id} = req.params;
             const {file} = req.files;
-            file.mv('uploads/filename.jpg', err => {
+            const extInfo = req.extInfo;
 
-                if (err) {
-                    return res.status(500).json({
+            User.findById(id, (errorFind, user) => {
+                if (errorFind || !user) {
+                    return res.status(404).json({
                         ok: false,
-                        message: "Something went wrong",
-                        error: err,
+                        message: 'The user does not exits',
+                        error: errorFind
                     })
                 }
 
-                return res.status(201).json({
-                    ok: true,
-                    message: "File uploaded!",
-                })
+                file.name = uniqid(`${id}-`, `.${extInfo.extension}`);
+
+                file.mv(`uploads/users/${file.name}`, err => {
+
+                    if (err) {
+                        return res.status(500).json({
+                            ok: false,
+                            message: "Something went wrong",
+                            error: err,
+                        })
+                    }
+
+                    return res.status(201).json({
+                        ok: true,
+                        message: "File uploaded!",
+                    })
+                });
             });
+
             break;
+
         case 'products':
             break;
+
         default:
             res.status(404).json({
                 ok: false,
